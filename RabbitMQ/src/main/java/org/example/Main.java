4c82439999ae7b9d79fb7d6137b217c79da22681
Mikayil Shahtahktinski <69395693+mshahcode@users.noreply.github.com>
2024-02-15 22:39:57 +0400

    Update Main.java

--- RabbitMQ/src/main/java/org/example/Main.java
+++ RabbitMQ/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 @SpringBootApplication
 public class Main {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
 }
